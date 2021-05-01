db5bd86997605b86cb7b8f62938aef40eede130d
iromashko <iromashko@me.com>
2021-05-01 03:21:25 +0300

    part 7

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,53 @@
+import {
+  HttpException,
+  HttpStatus,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { CreateUserDto } from 'src/users/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcryptjs';
+import { User } from 'src/users/user.model';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(dto: CreateUserDto) {
+    const user = await this.validateUser(dto);
+    return this.generateToken(user);
+  }
+
+  private async validateUser(dto: CreateUserDto) {
+    const user = await this.usersService.getUserByEmail(dto.email);
+    const passwordEquals = await bcrypt.compare(dto.password, user.password);
+    if (user && passwordEquals) {
+      return user;
+    }
+    throw new UnauthorizedException({ message: 'Invalid credentials' });
+  }
+
+  async registration(dto: CreateUserDto) {
+    const candidate = await this.usersService.getUserByEmail(dto.email);
+    if (candidate) {
+      throw new HttpException('User exist', HttpStatus.BAD_REQUEST);
+    }
+    const hashPassword = await bcrypt.hash(dto.password, 5);
+    const user = await this.usersService.createUser({
+      ...dto,
+      password: hashPassword,
+    });
+    return this.generateToken(user);
+  }
+
+  private async generateToken(user: User) {
+    const payload = { email: user.email, id: user.id, roles: user.roles };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+}
