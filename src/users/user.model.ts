c5fcf96108c4adfa8ba35b974cf55eb1ef4fab9b
iromashko <iromashko@me.com>
2021-05-01 11:13:11 +0300

    part 10

--- src/users/user.model.ts
+++ src/users/user.model.ts
@@ -5,7 +5,9 @@ import {
   DataType,
   Model,
   Table,
+  HasMany,
 } from 'sequelize-typescript';
+import { Post } from 'src/posts/posts.model';
 import { Role } from 'src/roles/roles.model';
 import { UserRoles } from 'src/roles/user-roles.model';
 
@@ -55,4 +57,7 @@ export class User extends Model<User, UserCreationAttrs> {
 
   @BelongsToMany(() => Role, () => UserRoles)
   roles: Role[];
+
+  @HasMany(() => Post)
+  posts: Post[];
 }
